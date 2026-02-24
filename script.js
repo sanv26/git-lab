945fe4b2c7980d0ee719dc237461f10c5c010216
parthg28-devops <parth.gujarkar23@spit.ac.in>
2026-02-24 10:58:40 +0530

    Added log(10) button

--- script.js
+++ script.js
@@ -19,3 +19,18 @@ function calculate() {
         display.value = "Error";
     }
 }
+
+function calcLog() {
+    try {
+        let value = parseFloat(display.value);
+
+        if (value <= 0 || isNaN(value)) {
+            display.value = "Error";
+            return;
+        }
+
+        display.value = Math.log10(value);
+    } catch {
+        display.value = "Error";
+    }
+}
